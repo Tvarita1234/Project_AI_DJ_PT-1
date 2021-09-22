c17ab558693f5ea46adbe106f32f8d65b8399464
Tvarita1234 <76855983+Tvarita1234@users.noreply.github.com>
2021-09-21 18:51:27 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+
+function preload()
+{
+   
+}
+
+
+function setup()
+{
+    canvas = createCanvas(500, 400);
+    canvas.center();
+
+    video = createCapture(VIDEO)
+    video.hide();
+}
+
+function draw()
+{
+    image(video, 0, 0, 600, 500);
+}
